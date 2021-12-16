5bf025fdb9e86221a010d04a3f5e07feda11f37e
lusajo143 <techdealers143@gmail.com>
2021-12-16 22:35:31 +0300

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { MediaChange, MediaObserver } from '@angular/flex-layout'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'youtube';
+
+  mediaSub: Subscription
+
+  deviceSmall: boolean
+
+  constructor(public mediaObsever: MediaObserver) {
+    this.mediaSub = this.mediaObsever.media$.subscribe((result:MediaChange) => {
+      console.log(result.mqAlias);
+      this.deviceSmall = result.mqAlias === 'xs' ? true : false
+    })
+  }
+
+  ngOnInit() {}
+
+  ngOnDestroy() {}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule }  from '@angular/material/button'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatInputModule } from '@angular/material/input';
+import { BodyComponent } from './body/body.component'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatDividerModule } from '@angular/material/divider'
+
+
+
+const Material = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  FlexLayoutModule,
+  MatInputModule,
+  MatSidenavModule,
+  MatListModule,
+  MatDividerModule
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    Material
   ],
   providers: [],
   bootstrap: [AppComponent]
